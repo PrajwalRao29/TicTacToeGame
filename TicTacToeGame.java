9c6aa2ecb72a52c4fb4549fdffd6f2d74947fc98
PrajwalRao29 <raoprajrao@gmail.com>
2020-10-01 12:30:18 +0530

    Added functionality to make player move

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -2,11 +2,13 @@ package com.cg.tictactoegame;
 
 import java.util.Scanner;
 
+import org.graalvm.compiler.lir.aarch64.AArch64Move.Move;
+
 public class TicTacToeGame {
 	static Scanner scanner = new Scanner(System.in);
 
 	/**
-	 * initializing the board
+	 * UC1 initializing the board
 	 */
 	public static char[] createBoard() {
 		char[] board = new char[10];
@@ -17,7 +19,7 @@ public class TicTacToeGame {
 	}
 
 	/**
-	 * @taking the player's input to choose between X and O
+	 * UC2 taking the player's input to choose between X and O
 	 */
 	public static char playerInput() {
 
@@ -32,22 +34,38 @@ public class TicTacToeGame {
 	}
 
 	/**
-	 * @Displaying the board
+	 * UC3 Displaying the board
 	 */
 	public static void showBoard(char[] board) {
+		
 		for (int position = 1; position < createBoard().length; position++) {
-			System.out.print(" | " + board[position]);
+			System.out.print("|  " + board[position]+" ");
 			if (position % 3 == 0) {
-				System.out.println(" |");
+				System.out.println("|");
+				
 			}
 		}
 	}
-
+	public static void move(char[] board,char player)
+	{
+		System.out.println("Enter the index to make a move");
+		int index=scanner.nextInt();
+		if(index>9 || index<1 ||board[index]!=' ')
+		{
+			System.out.println("Cannot play this index");
+			move(board,player);
+		}
+		else {
+			board[index]=player;
+		}
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe program");
 		char[] board = createBoard();
 		char player = playerInput();
 		char computer = (player == 'x') ? 'o' : 'x';
+		showBoard(board);
+		move(board,player);
 		showBoard(board);
 	}
 }
