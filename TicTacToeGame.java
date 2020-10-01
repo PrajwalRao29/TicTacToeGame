56237d0075f347bee33d2dfe6150bca7d2f05a7a
PrajwalRao29 <raoprajrao@gmail.com>
2020-10-01 13:12:28 +0530

    Added functionality to check if space is free to move

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -2,8 +2,6 @@ package com.cg.tictactoegame;
 
 import java.util.Scanner;
 
-import org.graalvm.compiler.lir.aarch64.AArch64Move.Move;
-
 public class TicTacToeGame {
 	static Scanner scanner = new Scanner(System.in);
 
@@ -37,26 +35,38 @@ public class TicTacToeGame {
 	 * UC3 Displaying the board
 	 */
 	public static void showBoard(char[] board) {
-		
+
 		for (int position = 1; position < createBoard().length; position++) {
-			System.out.print("|  " + board[position]+" ");
+			System.out.print("|  " + board[position] + " ");
 			if (position % 3 == 0) {
 				System.out.println("|");
-				
+
 			}
 		}
 	}
-	public static void move(char[] board,char player)
-	{
+
+	/**
+	 * UC4 Player move
+	 */
+	public static void move(char[] board, char player) {
 		System.out.println("Enter the index to make a move");
-		int index=scanner.nextInt();
-		if(index>9 || index<1 ||board[index]!=' ')
-		{
+		int index = scanner.nextInt();
+		if (index > 9 || index < 1 ||!isFree(board, index)) {
 			System.out.println("Cannot play this index");
-			move(board,player);
+			move(board, player);
+		} else {
+			board[index] = player;
 		}
-		else {
-			board[index]=player;
+	}
+
+	public static boolean isFree(char[] board, int index) {
+		if(board[index] !=' ')
+		{
+			System.out.println("index not free");
+		return false;
+		}
+		else{
+			return true;
 		}
 	}
 	public static void main(String[] args) {
@@ -65,7 +75,9 @@ public class TicTacToeGame {
 		char player = playerInput();
 		char computer = (player == 'x') ? 'o' : 'x';
 		showBoard(board);
-		move(board,player);
+		move(board, player);
+		showBoard(board);
+		move(board, player);
 		showBoard(board);
 	}
 }
